d1f285be9f8e558150d2764efa5bcb9902fe4613
Brian Litzinger <litzinger@gmail.com>
2020-01-10 14:55:22 -0600

    Add Cache class

---
+++ src/Cache.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Basee;
+
+/**
+ * @package     ExpressionEngine
+ * @category    Basee
+ * @author      Brian Litzinger
+ * @copyright   Copyright (c) 2016-2019 - Brian Litzinger
+ * @link        https://github.com/litzinger/basee
+ * @license     MIT
+ */
+
+class Cache
+{
+    /**
+     * @var object
+     */
+    private $driver;
+
+    /**
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * @var int
+     */
+    private $lifetime;
+
+    /**
+     * @param string $namespace
+     * @param int    $lifetime
+     */
+    public function __construct($namespace = '', $lifetime = 0)
+    {
+        $this->driver = ee()->cache;
+        $this->namespace = $namespace;
+        $this->lifetime = $lifetime;
+    }
+
+    /**
+     * @param  string $key
+     * @param bool $usePrefix
+     * @return mixed
+     */
+    public function get($key, $usePrefix = true)
+    {
+        return $this->driver->get($this->getKey($key, $usePrefix));
+    }
+
+    /**
+     * Save to cache
+     *
+     * @param  string $key
+     * @param  mixed $value
+     * @param null $lifetime
+     * @param bool $use_prefix
+     * @return null
+     */
+    public function save($key, $value, $lifetime = null, $use_prefix = true)
+    {
+        $lifetime = $lifetime ?? $this->lifetime;
+
+        $this->driver->save($this->getKey($key, $use_prefix), $value, $lifetime);
+    }
+
+    /**
+     * Bust the cache, either by key or all of it if no key is defined
+     *
+     * @param string $key
+     * @param bool $usePrefix
+     * @return $this|void
+     */
+    public function delete($key = null, $usePrefix = true)
+    {
+        if ($key && $usePrefix) {
+            $this->driver->delete($this->getKey($key, $usePrefix));
+        } elseif ($key) {
+            $this->driver->delete($this->namespace.'/'.$key.'/');
+        } else {
+            $this->driver->delete($this->namespace.'/');
+        }
+    }
+
+    /**
+     * Set the key prefix
+     *
+     * @param  string $key
+     * @param  bool $usePrefix
+     * @return string
+     */
+    private function getKey($key, $usePrefix = true)
+    {
+        if ($usePrefix) {
+            $prefix = $this->namespace;
+        }
+
+        return $prefix.'/'.$key;
+    }
+}
